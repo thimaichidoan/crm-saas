4a6024c45c3ab0acee2109254524ad074174c9bb
thimaichidoan <40016084@parisnanterre.fr>
2026-03-18 11:45:42 +0100

    add vercel frontend to cors

--- backend/crm_project/crm_project/settings.py
+++ backend/crm_project/crm_project/settings.py
@@ -104,6 +104,7 @@ AUTH_USER_MODEL = "users.User"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://crm-saas-git-main-thimaichidoans-projects.vercel.app",
 ]
 
 REST_FRAMEWORK = {
