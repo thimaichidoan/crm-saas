5602f5dc6b2394a3410799902295fe2181d6495c
thimaichidoan <40016084@parisnanterre.fr>
2026-03-19 09:37:28 +0100

    update api url fallback

--- frontend/crm-frontend/lib/api.ts
+++ frontend/crm-frontend/lib/api.ts
@@ -1,7 +1,7 @@
 // lib/api.ts
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_URL || "https://crm-saas-4bmp.onrender.com";
 
 export function getAuthHeaders() {
   if (typeof window === "undefined") {
